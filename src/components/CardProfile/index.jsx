a64214ccdb0e26b4e85a3e039b17ac13f44f1aea
vitorjaum <vitorjaum711@gmail.com>
2022-06-08 22:53:47 -0400

    try-add: cards prototype

--- src/components/CardProfile/index.jsx
+++ src/components/CardProfile/index.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
-const CardProfile = () => {
-  return <></>;
+const CardProfile = ({ info }) => {
+  const name = info.name;
+  const image = info.image;
+  const house = info.house;
+  const alt = `imagem de ${name}`;
+  let houseColor;
+
+  switch (house) {
+    case "Gryffindor":
+      houseColor = "red";
+      break;
+    case "Hufflepuff":
+      houseColor = "yellow";
+      break;
+    case "Slytherin":
+      houseColor = "green";
+      break;
+    case "Ravenclaw":
+      houseColor = "blue";
+      break;
+    default:
+      houseColor = "grey";
+  }
+
+  return (
+    <a style={{ backgroundColor: houseColor, marginRight: "2%" }}>
+      <div>
+        <img src={image} alt={alt} />
+        <p>{name}</p>
+      </div>
+    </a>
+  );
 };
 
 export default CardProfile;
--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,11 +1,58 @@
 import React from "react";
+import characters from "../../contants/index";
+import CardProfile from "../CardProfile";
 
-const Card = () => {
+const tempArray = [
+  {
+    name: "Hermione Granger",
+    image: "http://hp-api.herokuapp.com/images/hermione.jpeg",
+    house: "Gryffindor",
+  },
+  {
+    name: "Hermione Granger",
+    image: "http://hp-api.herokuapp.com/images/hermione.jpeg",
+    house: "Gryffindor",
+  },
+  {
+    name: "Hermione Granger",
+    image: "http://hp-api.herokuapp.com/images/hermione.jpeg",
+    house: "Gryffindor",
+  },
+];
+
+const Cards = () => {
+  const [cards, setCards] = React.useState(tempArray);
+
+  function getCards() {
+    const newObj = {
+      name: "Hermione Granger",
+      image: "http://hp-api.herokuapp.com/images/hermione.jpeg",
+      house: "Gryffindor",
+    };
+    const newArr = [...tempArray, newObj];
+    tempArray.push(newObj);
+
+    setCards(newArr);
+    console.log({ tempArray });
+  }
+
   return (
-    <div>
-      <p>cards</p>
+    <div style={{ padding: "2% 5%" }}>
+      <div
+        style={{
+          display: "flex",
+          margin: "0 auto",
+          flexWrap: "wrap",
+        }}
+        id={"card"}
+      >
+        {cards.map((id) => (
+          <CardProfile info={id} />
+        ))}
+      </div>
+      <button onClick={getCards}>Exibir Mais...</button>
     </div>
   );
 };
 
-export default Card;
+export default Cards;
